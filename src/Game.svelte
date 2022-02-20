6300f13b640465affe59992f81ab42ff44dae6a4
Colin Bate <colin@colinbate.com>
2022-02-19 22:09:16 -0400

    Random starter with proper alternation

--- src/Game.svelte
+++ src/Game.svelte
@@ -2,12 +2,15 @@
   import { PLAYERS } from "./constants";
   import Grid from "./Grid.svelte";
   import Scoreboard from "./Scoreboard.svelte";
-  import type { WinEvent } from "./types";
+  import type { PlayerId, WinEvent } from "./types";
 
-  let turn = 0;
+  let starter: PlayerId = Math.floor(Math.random() * PLAYERS);
+  let turn: PlayerId = starter;
   let scores = Array(PLAYERS).fill(0);
   function handleWin(ev: WinEvent) {
     scores[ev.detail.player] += 1;
+    starter = (starter + 1) % PLAYERS;
+    turn = starter;
   }
 </script>
 <Grid on:win={handleWin} bind:turn />
