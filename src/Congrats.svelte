0578f50bc74e32e98717d5580515926bccfd7d3d
Colin Bate <colin@colinbate.com>
2022-02-19 23:21:09 -0400

    Display winner message

---
+++ src/Congrats.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { ROWS } from "./constants";
+  import Token from "./Token.svelte";
+  import type { PlayerId } from "./types";
+  import { fly } from 'svelte/transition';
+
+  export let winner: PlayerId;
+  
+</script>
+<div class="absolute inset-0 flex items-center justify-center z-10">
+  <div transition:fly={{y: -50}} class="absolute w-1/2 rounded-xl p-8 dark:bg-gray-700 shadow bg-gray-300 flex flex-col gap-4 items-center justify-center">
+    <div class="flex items-center justify-center w-full gap-4">
+      <div class="w-1/4"><Token token={winner} pos={ROWS} /></div>
+      <span class="text-3xl">You win!</span>
+    </div>
+    <button on:click type="button" class="rounded py-3 px-6 bg-svelte hover:bg-gradient-to-t from-[rgba(0,0,0,0.3)]">Play Again</button>
+  </div>
+</div>
--- src/Grid.svelte
+++ src/Grid.svelte
@@ -1,31 +1,41 @@
 <script lang="ts">
   import Column from "./Column.svelte";
+  import Congrats from "./Congrats.svelte";
   import {createEventDispatcher} from 'svelte';
   import type { Board, PlayerId } from "./types";
   import { COLS, PLAYERS, ROWS } from "./constants";
   import { checkWin } from "./winning";
 
   export let turn: PlayerId;
-  
+
   const dispatch = createEventDispatcher();
 
   const empty = () => Array.from(Array(COLS), () => ([]));
   let board: Board = empty();
+  let winner: PlayerId | undefined;
 
   function handleClick(index: number) {
-    if (board[index].length === ROWS) {
+    if (winner || board[index].length === ROWS) {
       return;
     }
     board[index] = [...board[index], turn];
     if (checkWin(board, index)) {
-      dispatch('win', { player: turn });
-      board = empty();
+      winner = turn;
+      dispatch('win', { player: winner });
     } else {
       turn = (turn + 1) % PLAYERS;
     }
   }
+
+  function playAgain() {
+    winner = undefined;
+    board = empty();
+  }
 </script>
-<div class="w-full flex border-black dark:border-white border-l">
+<div class="w-full flex border-black dark:border-white border-l relative">
+  {#if winner != null}
+  <Congrats {winner} on:click={playAgain} />
+  {/if}
   {#each board as column, i}
     <Column {column} on:click={() => handleClick(i)} />
   {/each}
