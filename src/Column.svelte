5472b454ce149d0696681c3f8c72b7d54356e3dc
Colin Bate <colin@colinbate.com>
2022-02-19 21:27:14 -0400

    Use cursor to indicate valid click targets.

--- src/Column.svelte
+++ src/Column.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-import { ROWS } from "./constants";
-
+  import { ROWS } from "./constants";
   import Token from "./Token.svelte";
-import type { Column } from "./types";
+  import type { Column } from "./types";
   export let column: Column;
   $: fullColumn = Array.from({...column, length: ROWS});
+  $: isFull = column.length === ROWS;
 </script>
-<div on:click class="flex-grow flex flex-col-reverse border-r border-black dark:border-white">
+<div on:click class:cursor-not-allowed={isFull} class="cursor-pointer flex-grow flex flex-col-reverse border-r border-black dark:border-white">
   {#each fullColumn as token, i}
     <Token {token} pos={i} />
   {/each}
